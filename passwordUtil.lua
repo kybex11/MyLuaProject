70d1116a04532b7ecc360d7f744dc39f90b9f878
varnet11 <113356445+varnet11@users.noreply.github.com>
2022-11-01 12:45:50 +0300

    Add files via upload

---
+++ passwordUtil.lua
@@ -0,0 +1,15 @@
+print("Create new password")
+password = io.read()
+print("Password created")
+
+print("Enter password")
+pass = io.read()
+
+if (pass == password) then
+    print("Password corrected")
+ else
+    print("Password incorrect")
+    print("Try again")
+ end
+ 
+
---
+++ quizNumber.lua
@@ -0,0 +1,12 @@
+local d = "289749"
+
+print("Hello this is quiz Number Game!")
+print("Quiz number")
+quiznum = io.read()
+
+if (quiznum == d) then
+     print("YESS YOU WIN 1000$")
+     print("YOUR MONEY 1000$")
+else
+    print("NOOOOOO Try again!")
+end
